6b3a7b110e8c43c65eac3f2ce37f9d7136954454
hoangvunghi <hoangvunghi2004@gmail.com>
2025-03-21 10:31:15 +0700

    alter url

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import login, register, token_refresh,forgot_password_view,reset_password_view
 from . import views
 urlpatterns = [
-    path('api/register/', register, name='register'),
-    path('api/login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
     path('activate/<str:token>/', views.activate_account, name='activate_account'),
-    path('api/token-refresh/', token_refresh, name='token-refresh'),
-    path('api/forgot-password/', forgot_password_view, name='forgot-password'),
-    path('api/reset-password/', reset_password_view, name='reset-password'),
+    path('token-refresh/', token_refresh, name='token-refresh'),
+    path('forgot-password/', forgot_password_view, name='forgot-password'),
+    path('reset-password/', reset_password_view, name='reset-password'),
     path('resend-activation/', views.resend_activation_email, name='resend_activation'),
 ]
